11505b1821ccf38ea569434d0926842780de0b16
Jeremy Brown <jeremybrown@nc.rr.com>
2015-05-08 13:48:48 -0500

    base setup

---
+++ AzureTableStorageRepository/Domain/Interfaces/Models/IContact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Interfaces.Models
+{
+    public interface IContact
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string EmailAddress { get; set; }
+    }
+}
---
+++ AzureTableStorageRepository/Domain/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain.Interfaces.Models;
+
+namespace Domain.Models
+{
+    public class Contact : IContact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
